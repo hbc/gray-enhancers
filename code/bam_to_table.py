7a352e7b70dc9e81fc9a057f22d1ddfc46049fd8
roryk <rory.kirchner@gmail.com>
2014-11-03 13:22:48 -0500

    Fixes to get working on Orchestra.

--- code/bam_to_table.py
+++ code/bam_to_table.py
@@ -3,8 +3,28 @@ from bcbio.bam import open_samfile
 import pysam
 
 
-OUT_HEADER = ["id", "barcode", "eid", "species", "subtype", "mismatch", "mapq", "as", "xs", "cigar"]
+OUT_HEADER = ["id", "barcode", "eid", "species", "subtype", "mismatch", "mapq", "as", "xs", 
+              "clipped", "insertions", "deletions", "matched", "other"]
 
+def parse_cigar_tuples(tuples):
+    d = {"insertions": 0,
+         "deletions": 0,
+         "clipped": 0,
+         "matched": 0,
+         "other": 0}
+    for t in tuples:
+        if t[0] == 4 or t[0] == 5:
+            d["clipped"] += t[1]
+        elif t[0] == 1:
+            d["insertions"] += t[1]
+        elif t[0] == 2:
+            d["deletions"] += t[1]
+        elif t[0] == 0:
+            d["matched"] += t[1]
+        else:
+            d["other"] += t[1]
+
+    return d
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -39,6 +59,12 @@ if __name__ == "__main__":
                 XS = read.opt("XS")
             except KeyError:
                 XS = 0
-	    cigar = read.cigarstring
-            out_line = map(str, [number, barcode, eid, species, subtype, mismatch, mapq, AS, XS, cigar])
+    	    cigar = parse_cigar_tuples(read.cigar)
+            clipped = cigar["clipped"]
+            insertions = cigar["insertions"]
+            deletions = cigar["deletions"]
+            matched = cigar["matched"]
+            other = cigar["other"]
+            out_line = map(str, [number, barcode, eid, species, subtype, mismatch, mapq, AS, XS, 
+                                 clipped, insertions, deletions, matched, other])
             print "\t".join(out_line)
