0957d2daeefed0838ed9047893d7c32bf08be2f3
roryk <rory.kirchner@gmail.com>
2015-07-26 13:00:12 -0400

    Drop duplicated enhancer sequences when trimming.

--- code/trim_enhancers.py
+++ code/trim_enhancers.py
@@ -9,15 +9,24 @@ to
 
 >-249753741_hs_Endogenous_chr1:6052584^WCGCGTYat-59^GATTGGYat-29^GATTGGYat14^YGCGGCKSat23
 aactccccagcagcctgtacgtttagtcctacccgggcCCGCCGCAggGATTGGCaccgcgagcgtttcgcgtcgggagctgaacccgagaGATTGGCaggcgccgggactgccgctgtcaGACGCGAccgcccaaga
+
+this keeps only unique sequences
 """
 
+from __future__ import print_function
 import sys
+import itertools
 
-print sys.argv[1]
+seen = set()
+
 with open(sys.argv[1], "r") as in_handle:
-    for line in in_handle:
-        if line.startswith(">"):
-            print line.strip()
+    for name, seq in itertools.izip_longest(*[in_handle]*2):
+        llen = len(seq)
+        piece = seq[16:(llen-16)].strip()
+        if piece not in seen:
+            seen.update([piece])
+            print(name.strip(), file=sys.stdout)
+            print(piece, file=sys.stdout)
         else:
-            llen = len(line)
-            print line[16:(llen-16)].strip()
+            print("%s sequence already seen, skipping %s." % (seq, name),
+                  file=sys.stderr)
